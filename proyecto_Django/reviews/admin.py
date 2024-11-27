ba509ffcfe01d3f1037d978883bf8909f960d62b
AntonyCondori <acondoricai@unsa.edu.pe>
2024-11-27 01:18:19 -0500

    Registrando los modelos

--- proyecto_Django/reviews/admin.py
+++ proyecto_Django/reviews/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Review
+from .models import Book
+
+admin.site.register(Review)
+admin.site.register(Book)
--- proyecto_Django/reviews/models.py
+++ proyecto_Django/reviews/models.py
@@ -5,7 +5,6 @@ class Book(models.Model):
     title = models.CharField(max_length=255)
     author = models.CharField(max_length=255)
     published_date = models.DateField()
-    isbn = models.CharField(max_length=13, unique=True)
 
     def __str__(self):
         return self.title
