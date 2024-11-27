77717b90f7436045b5e212d0fa3878d1806dcd35
AntonyCondori <acondoricai@unsa.edu.pe>
2024-11-26 23:42:49 -0500

    Definicion de modelos para libros y reviews, Creacion de migraciones para los modelos

--- proyecto_Django/reviews/models.py
+++ proyecto_Django/reviews/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    published_date = models.DateField()
+    isbn = models.CharField(max_length=13, unique=True)
+
+    def __str__(self):
+        return self.title
+
+class Review(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name="reviews")
+    user = models.CharField(max_length=100)
+    rating = models.IntegerField(choices=[(i, str(i)) for i in range(1, 6)])
+    comment = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user} - {self.book.title}"
